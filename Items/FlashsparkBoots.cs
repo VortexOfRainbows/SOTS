dcc116e97e88fa865b15dbcd06d27ed51ad9ea61
Vortex <Thederpsnail@gmail.com>
2021-08-11 22:10:29 -0700

    Recipro Burst

--- Items/FlashsparkBoots.cs
+++ Items/FlashsparkBoots.cs
@@ -32,6 +32,8 @@ namespace SOTS.Items
 			recipe.SetResult(this);
 			recipe.AddRecipe();
 		}
+        bool activateParticle = false;
+        bool hasActivate = false;
 		public override void UpdateAccessory(Player player, bool hideVisual)
 		{
 			player.buffImmune[BuffID.Burning] = true;
@@ -43,21 +45,71 @@ namespace SOTS.Items
 			player.moveSpeed += 0.2f;
             player.accRunSpeed = 7f;
             bool particles = FireBoostPlayer(player);
-            if (particles)
+            if(activateParticle)
+            {
+                if(!hasActivate)
+                {
+                    Main.PlaySound(SoundID.Item, (int)player.Center.X, (int)player.Center.Y, 45, 1.3f, -0.4f);
+                    Main.PlaySound(SoundID.Item, (int)player.Center.X, (int)player.Center.Y, 14, 1.0f, -0.3f);
+                    hasActivate = true;
+                    for(int i = 0; i < 3; i++)
+                    {
+                        int amt = 45 + i * 15;
+                        for (int j = 0; j < amt; j++)
+                        {
+                            Vector2 circularLocation = new Vector2(30, 0).RotatedBy(MathHelper.ToRadians(360f * j / amt));
+                            circularLocation.X *= 0.6f;
+                            var index = Dust.NewDust(player.Center + circularLocation - new Vector2(5), 0, 0, DustID.Fire, -player.velocity.X * 1.5f, player.velocity.Y * 0.5f, 50, new Color(), 5f - i * 0.8f);
+                            circularLocation = circularLocation.SafeNormalize(Vector2.Zero) * 7.5f;
+                            circularLocation.X -= player.velocity.X * (1.5f + (i * 0.5f));
+                            if(i == 0)
+                            {
+                                circularLocation *= 0.33f;
+                            }
+                            if (i == 1)
+                            {
+                                circularLocation *= 0.66f;
+                            }
+                            Main.dust[index].velocity.X += circularLocation.X;
+                            Main.dust[index].velocity.Y = circularLocation.Y;
+                            Main.dust[index].noGravity = true;
+                            Main.dust[index].shader = GameShaders.Armor.GetSecondaryShader(player.cShoe, player);
+                        }
+                    }
+                    player.velocity.X *= 2.2f;
+                }
+            }
+            else
+            {
+                hasActivate = false;
+            }
+            if (particles && doAccel)
             {
                 player.accRunSpeed = 0f;
                 player.velocity *= 1 / 0.96f;
             }
         }
+        bool doAccel = false;
 		public bool FireBoostPlayer(Player player)
         {
+            doAccel = false;
+            bool doAccel1 = false;
+            bool doAccel2 = false;
             float doubleRun = player.runAcceleration * 2.5f;
-            float doubleAcc = 16f;
+            float doubleAcc = 14f;
             bool doDust = false;
             bool otherDust = false;
             float num1 = (doubleAcc + player.maxRunSpeed) / 1.4f;
             if (player.controlLeft && player.velocity.X > -doubleAcc && player.dashDelay >= 0)
             {
+                if (player.mount.Active && player.mount.Cart)
+                {
+                    if (player.velocity.X < 0.0)
+                        player.direction = -1;
+                }
+                else if ((player.itemAnimation == 0 || player.inventory[player.selectedItem].useTurn) && player.mount.AllowDirectionChange)
+                    player.direction = -1;
+                doAccel1 = true;
                 float speedMod = 0.25f;
                 if(player.velocity.X < -player.accRunSpeed && player.velocity.Y == 0.0)
                 {
@@ -71,13 +123,17 @@ namespace SOTS.Items
                     if (player.wingsLogic > 0)
                         player.velocity.X -= doubleRun * 0.2f * speedMod;
                 }
-                if (player.velocity.X < -num1 && player.velocity.Y == 0.0 && !player.mount.Active)
-                {
-                    doDust = true;
-                }
             }
             else if (player.controlRight && player.velocity.X < doubleAcc && player.dashDelay >= 0)
             {
+                if (player.mount.Active && player.mount.Cart)
+                {
+                    if (player.velocity.X > 0.0)
+                        player.direction = -1;
+                }
+                else if ((player.itemAnimation == 0 || player.inventory[player.selectedItem].useTurn) && player.mount.AllowDirectionChange)
+                    player.direction = 1;
+                doAccel2 = true;
                 float speedMod = 0.25f;
                 if (player.velocity.X > player.accRunSpeed && player.velocity.Y == 0.0)
                 {
@@ -91,9 +147,35 @@ namespace SOTS.Items
                     if (player.wingsLogic > 0)
                         player.velocity.X += doubleRun * 0.2f * speedMod;
                 }
-                if (player.velocity.X > num1 && player.velocity.Y == 0.0 && !player.mount.Active)
+            }
+            if (player.velocity.X < -num1 && player.velocity.Y == 0.0 && !player.mount.Active)
+            {
+                doDust = true;
+                if (doAccel1 && !doAccel2)
                 {
-                    doDust = true;
+                    if (player.mount.Active && player.mount.Cart)
+                    {
+                        if (player.velocity.X < 0.0)
+                            player.direction = -1;
+                    }
+                    else if ((player.itemAnimation == 0 || player.inventory[player.selectedItem].useTurn) && player.mount.AllowDirectionChange)
+                        player.direction = -1;
+                    doAccel = true;
+                }
+            }
+            if (player.velocity.X > num1 && player.velocity.Y == 0.0 && !player.mount.Active)
+            {
+                doDust = true;
+                if (doAccel2 && !doAccel1)
+                {
+                    if (player.mount.Active && player.mount.Cart)
+                    {
+                        if (player.velocity.X > 0.0)
+                            player.direction = -1;
+                    }
+                    else if ((player.itemAnimation == 0 || player.inventory[player.selectedItem].useTurn) && player.mount.AllowDirectionChange)
+                        player.direction = 1;
+                    doAccel = true;
                 }
             }
             if (doDust || otherDust)
@@ -106,12 +188,12 @@ namespace SOTS.Items
                 {
                     if(!doDust)
                     {
-                        Main.PlaySound(player.hermesStepSound.SoundType, (int)player.position.X, (int)player.position.Y, player.hermesStepSound.SoundStyle, 1f, -0.075f);
+                        Main.PlaySound(player.hermesStepSound.SoundType, (int)player.Center.X, (int)player.Center.Y, player.hermesStepSound.SoundStyle, 1f, -0.075f);
                         player.runSoundDelay = (int)(player.hermesStepSound.IntendedCooldown / 1.25f);
                     }    
                     else
                     {
-                        Main.PlaySound(player.hermesStepSound.SoundType, (int)player.position.X, (int)player.position.Y, player.hermesStepSound.SoundStyle, 1f, 0.125f);
+                        Main.PlaySound(player.hermesStepSound.SoundType, (int)player.Center.X, (int)player.Center.Y, player.hermesStepSound.SoundStyle, 1f, 0.125f);
                         player.runSoundDelay = (int)(player.hermesStepSound.IntendedCooldown / 1.6f);
                     }
                 }
@@ -121,10 +203,12 @@ namespace SOTS.Items
                     Main.dust[index].velocity.X *= 0.2f;
                     Main.dust[index].velocity.Y *= 0.2f;
                     Main.dust[index].shader = GameShaders.Armor.GetSecondaryShader(player.cShoe, player);
+                    activateParticle = false;
                 }
                 else
                 {
-                    for(float i = 0; i <= 3; i += 0.5f)
+                    activateParticle = true;
+                    for (float i = 0; i <= 3; i += 0.5f)
                     {
                         int length = 1;
                         if (Main.rand.NextBool(10))
