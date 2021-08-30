45dc40659b9ca16efed19a3b03b43f929742c40e
Vortex <Thederpsnail@gmail.com>
2021-08-29 21:39:21 -0700

    Frost Artifact Resprite

--- Items/IceStuff/FrostArtifactChestplate.cs
+++ Items/IceStuff/FrostArtifactChestplate.cs
@@ -6,54 +6,37 @@ namespace SOTS.Items.IceStuff
 {
 	[AutoloadEquip(EquipType.Body)]
 	public class FrostArtifactChestplate : ModItem
-	{	int Probe = -1;
+	{
 		public override void SetDefaults()
 		{
-
-			item.width = 30;
-			item.height = 20;
-
+			item.width = 38;
+			item.height = 22;
             item.value = Item.sellPrice(0, 6, 50, 0);
-			item.rare = 7;
+			item.rare = ItemRarityID.Lime;
 			item.defense = 24;
 		}
-
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Frost Artifact Breastplate");
-			Tooltip.SetDefault("10% increased melee and ranged critical strike chance\nA Frost Spike rotates around you");
+			Tooltip.SetDefault("16% increased melee and ranged critical strike chance\nA Frost Spike rotates around you");
 		}
 		public override bool IsArmorSet(Item head, Item body, Item legs)
         {
             return head.type == mod.ItemType("FrostArtifactHelmet") && legs.type == mod.ItemType("FrostArtifactTrousers");
         }
-
 		public override void UpdateEquip(Player player)
 		{
-			player.meleeCrit += 10;
-			player.rangedCrit += 10;
-			if (Main.myPlayer == player.whoAmI)
-			{
-				if (Probe == -1)
-				{
-					Probe = Projectile.NewProjectile(player.Center.X, player.Center.Y - 150, 0, 0, mod.ProjectileType("FrostSpike"), 60, 1, player.whoAmI);
-				}
-				if (!Main.projectile[Probe].active || Main.projectile[Probe].type != mod.ProjectileType("FrostSpike"))
-				{
-					Probe = Projectile.NewProjectile(player.Center.X, player.Center.Y - 150, 0, 0, mod.ProjectileType("FrostSpike"), 60, 1, player.whoAmI);
-				}
-				Main.projectile[Probe].timeLeft = 6;
-			}
+			player.meleeCrit += 16;
+			player.rangedCrit += 16;
 		}
 		public override void AddRecipes()
 		{
 			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(685, 1);
+			recipe.AddIngredient(ItemID.FrostBreastplate, 1);
 			recipe.AddIngredient(null, "AbsoluteBar", 24);
 			recipe.SetResult(this);
 			recipe.AddTile(TileID.MythrilAnvil);
 			recipe.AddRecipe();
 		}
-
 	}
 }
--- Items/IceStuff/FrostArtifactTrousers.cs
+++ Items/IceStuff/FrostArtifactTrousers.cs
@@ -6,54 +6,37 @@ namespace SOTS.Items.IceStuff
 {
 	[AutoloadEquip(EquipType.Legs)]
 	public class FrostArtifactTrousers : ModItem
-	{	int Probe = -1;
+	{
 		public override void SetDefaults()
 		{
-
-			item.width = 22;
-			item.height = 18;
-			
+			item.width = 24;
+			item.height = 20;
             item.value = Item.sellPrice(0, 6, 25, 0);
-			item.rare = 7;
-			item.defense = 15;
+			item.rare = ItemRarityID.Lime;
+			item.defense = 16;
 		}
-
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Frost Artifact Trousers");
-			Tooltip.SetDefault("10% increased melee and movement speed\nSummons a Frost Probe above you that assists in combat");
+			Tooltip.SetDefault("10% increased melee and movement speed");
 		}
 		public override bool IsArmorSet(Item head, Item body, Item legs)
         {
             return body.type == mod.ItemType("FrostArtifactChestplate") && head.type == mod.ItemType("FrostArtifactHelmet");
         }
-
 		public override void UpdateEquip(Player player)
 		{
 			player.moveSpeed += 0.1f;
 			player.meleeSpeed += 0.1f;
-			if (Main.myPlayer == player.whoAmI)
-			{
-				if (Probe == -1)
-				{
-					Probe = Projectile.NewProjectile(player.Center.X, player.Center.Y - 150, 0, 0, mod.ProjectileType("FrostProbe"), 60, 1, player.whoAmI);
-				}
-				if (!Main.projectile[Probe].active || Main.projectile[Probe].type != mod.ProjectileType("FrostProbe"))
-				{
-					Probe = Projectile.NewProjectile(player.Center.X, player.Center.Y - 150, 0, 0, mod.ProjectileType("FrostProbe"), 60, 1, player.whoAmI);
-				}
-				Main.projectile[Probe].timeLeft = 6;
-			}
 		}
 		public override void AddRecipes()
 		{
 			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(686, 1);
+			recipe.AddIngredient(ItemID.FrostLeggings, 1);
 			recipe.AddIngredient(null, "AbsoluteBar", 20);
 			recipe.SetResult(this);
 			recipe.AddTile(TileID.MythrilAnvil);
 			recipe.AddRecipe();
 		}
-
 	}
 }
