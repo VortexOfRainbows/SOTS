0bca7eaaf8f65493385e3d4acab3eb63c6eda4f2
GraydeeSi <wiseguy0002@gmail.com>
2021-10-16 21:20:59 -0400

    Hopefully fixed issue? May be some overhead

--- Prim/PrimBase.cs
+++ Prim/PrimBase.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
 using System.Linq;
+using System.Reflection;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -28,6 +29,10 @@ namespace SOTS.Prim
 
 			gD.SetRenderTarget(primTargetNPC);
 			gD.Clear(Color.Transparent);
+
+			if ((bool)typeof(SpriteBatch).GetField("beginCalled", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(spriteBatch))
+				spriteBatch.End();
+
 			spriteBatch.Begin();
 			foreach (PrimTrail trail in _trails.ToArray().Where(x => x.DrawType == DrawNPC)) {
 				if (trail.Pixellated && !trail.Disabled)
@@ -51,6 +56,10 @@ namespace SOTS.Prim
 
 			gD.SetRenderTarget(primTargetProjectile);
 			gD.Clear(Color.Transparent);
+
+			if ((bool)typeof(SpriteBatch).GetField("beginCalled", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(spriteBatch))
+				spriteBatch.End();
+
 			spriteBatch.Begin();
 			foreach (PrimTrail trail in _trails.ToArray().Where(x => x.DrawType == DrawProjectile)) {
 				if (trail.Pixellated && !trail.Disabled)
