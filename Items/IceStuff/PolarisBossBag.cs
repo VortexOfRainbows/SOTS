494af5222b0414e9497dcc580baf1349e18db349
Delta-official <64544951+Delta-official@users.noreply.github.com>
2021-11-20 00:29:34 +0300

    Update PolarisBossBag.cs

--- Items/IceStuff/PolarisBossBag.cs
+++ Items/IceStuff/PolarisBossBag.cs
@@ -19,7 +19,7 @@ namespace SOTS.Items.IceStuff
 			item.value = 0;
 			item.rare = ItemRarityID.Cyan;
 			item.expert = true;
-			item.maxStack = 99;
+			item.maxStack = 999;
 			item.consumable = true;
 		}
 		public override int BossBagNPC => ModContent.NPCType<Polaris>();
