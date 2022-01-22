d2dae48a193020337f00b427f165855a9f5ac737
Vortex <Thederpsnail@gmail.com>
2022-01-21 23:50:08 -0800

    Chaos Spirit wingies

--- NPCs/Constructs/ChaosConstruct.cs
+++ NPCs/Constructs/ChaosConstruct.cs
@@ -51,57 +51,76 @@ namespace SOTS.NPCs.Constructs
 		{
 			Vector2 origin = new Vector2(npc.width / 2, npc.height / 2);
 			Texture2D texture = Main.npcTexture[npc.type];
-			dir = (float)Math.Atan2(aimTo.Y - npc.Center.Y, aimTo.X - npc.Center.X) - MathHelper.PiOver2;
 			DrawWings();
 			Main.spriteBatch.Draw(texture, npc.Center - Main.screenPosition + new Vector2(0, npc.gfxOffY), null, drawColor, dir, origin, npc.scale, SpriteEffects.None, 0f);
 			Main.spriteBatch.Draw(ModContent.GetTexture("SOTS/NPCs/Constructs/ChaosConstructGlow"), npc.Center - Main.screenPosition + new Vector2(0, npc.gfxOffY), null, Color.White, dir, origin, npc.scale, SpriteEffects.None, 0f);
 			return false;
 		}
+		float counter1 = 0;
 		float counter2 = 0;
 		float lastWingHeight = 0; //wings offset in degrees
 		float wingHeight; //wings offset in degrees
 		public void WingStuff()
 		{
-			counter2 += 6.5f;
+			counter2 += 7.5f;
 			float dipAndRise = (float)Math.Sin(MathHelper.ToRadians(counter2));
 			//dipAndRise *= (float)Math.sqrt(dipAndRise);
-			wingHeight = 30 + dipAndRise * 20;
+			wingHeight = 19 + dipAndRise * 27;
 			lastWingHeight = wingHeight;
 		}
 		public void DrawWings()
         {
 			Texture2D texture = ModContent.GetTexture("SOTS/NPCs/Constructs/ChaosParticle");
 			Vector2 origin = new Vector2(texture.Width / 2, texture.Height / 2);
-			int width = 140;
-			int height = 100;
-			int amtOfParticles = 100;
+			float dipAndRise = (float)Math.Sin(MathHelper.ToRadians(counter2));
+			int width = 130;
+			int height = 90;
+			int amtOfParticles = 120;
 			for(int j = -1; j <= 1; j+= 2)
 			{
-				float positionalRotation = npc.rotation + MathHelper.ToRadians(wingHeight * -j) - MathHelper.PiOver2;
-				Vector2 position = npc.Center + new Vector2(((width + npc.width) / 2 - 28) * j, 10).RotatedBy(positionalRotation);
-				float degreesCount = 0;
+				float positionalRotation = npc.rotation + MathHelper.ToRadians(wingHeight * -j);
+				Vector2 position = npc.Center + new Vector2(-28 * j, 20).RotatedBy(npc.rotation) + new Vector2((width + npc.width) / 2 * j, 0).RotatedBy(positionalRotation);
+				float degreesCount = 0f;
+				float flapMult = 0.0f;
+				float baseGrowth = 0.35f;
+				float scaleGrowth = 0.15f;
+				float totalSin = 450f;
+				if (dipAndRise < 0)
+				{
+					baseGrowth = MathHelper.Lerp(baseGrowth, 0.2f, -dipAndRise);
+					scaleGrowth = MathHelper.Lerp(scaleGrowth, 0.3f, -dipAndRise);
+					totalSin = MathHelper.Lerp(totalSin, 405, (float)Math.Pow((-dipAndRise), 1.2f));
+				}
+				else
+				{
+					baseGrowth = MathHelper.Lerp(baseGrowth, 0.1f, dipAndRise);
+					scaleGrowth = MathHelper.Lerp(scaleGrowth, 0.05f, dipAndRise);
+					flapMult = MathHelper.Lerp(scaleGrowth, 0.3f, (float)Math.Pow(dipAndRise, 1.2f));
+				}
 				for (float i = 0; i < amtOfParticles;)
 				{
 					float sinusoid = (float)Math.Sin(MathHelper.ToRadians(degreesCount));
+					if (degreesCount < 0)
+						sinusoid = 0;
 					float radians = MathHelper.ToRadians(i * 360f / amtOfParticles);
 					Color c = VoidPlayer.pastelAttempt(radians + MathHelper.ToRadians(Main.GameUpdateCount));
 					Vector2 circular = new Vector2(-1, 0).RotatedBy(radians);
 					float increaseAmount = 1f;
-					if(i < amtOfParticles / 2)
+					if (i < amtOfParticles / 2)
                     {
-						degreesCount += 450f / amtOfParticles * 2f;
-						circular.Y *= 0.0f;
-						circular.Y -= sinusoid * (0.3f + 0.2f * i / amtOfParticles);
+						degreesCount = MathHelper.Lerp(0, totalSin, (float)Math.Pow(i / amtOfParticles * 2f, 1.2f));
+						circular.Y *= flapMult;
+						circular.Y -= sinusoid * (baseGrowth + scaleGrowth * i / amtOfParticles);
                     }	
 					else
                     {
-						float mult = (1 - (i - 50f) / amtOfParticles * 7f);
+						float mult = (1 - (i - 60f) / amtOfParticles * 7f);
 						if (mult < 0)
 							mult = 0;
-						circular.Y -= 0.5f * mult;
+						circular.Y -= (float)Math.Sin(MathHelper.ToRadians(totalSin)) * (scaleGrowth + baseGrowth) * mult;
 						if(circular.Y > 0)
                         {
-							sinusoid = (float)Math.Sin(MathHelper.ToRadians(Main.GameUpdateCount * 12f + i * 36));
+							sinusoid = (float)Math.Sin(MathHelper.ToRadians(Main.GameUpdateCount * 20f + i * 36));
 							if (sinusoid < 0.0f)
 							{
 								if (sinusoid > -0.2f)
@@ -116,7 +135,7 @@ namespace SOTS.NPCs.Constructs
                             {
 								increaseAmount = 0.25f;
                             }
-							circular.Y *= 1f + sinusoid * 0.6f;
+							circular.Y *= 1f + sinusoid * 0.4f;
                         }
 					}
 					i += increaseAmount;
@@ -161,11 +180,13 @@ namespace SOTS.NPCs.Constructs
 			Lighting.AddLight(npc.Center, (255 - npc.alpha) * 0.45f / 155f, (255 - npc.alpha) * 0.25f / 155f, (255 - npc.alpha) * 0.45f / 155f);
 			Vector2 toPlayer = player.Center - npc.Center;
 			dir = (float)Math.Atan2(aimTo.Y - npc.Center.Y, aimTo.X - npc.Center.X);
-			npc.rotation = dir;
+			//npc.rotation = dir;
 			if(npc.ai[0] == 0)
             {
+				npc.rotation = npc.velocity.X * 0.06f;
 				DoPassiveMovement();
             }
+			dir = npc.rotation;
 		}
 		public void DoPassiveMovement()
 		{
@@ -192,7 +213,7 @@ namespace SOTS.NPCs.Constructs
 			Main.npc[n].velocity.Y = -10f;
 			if (Main.netMode != NetmodeID.MultiplayerClient)
 				Main.npc[n].netUpdate = true;
-			Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ModContent.ItemType<FragmentOfInferno>(), Main.rand.Next(4) + 4);
+			Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ModContent.ItemType<FragmentOfChaos>(), Main.rand.Next(4) + 4);
 		}	
 	}
 }
