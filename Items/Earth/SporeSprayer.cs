cc0ec160966a72f2c1f13bc9535bc877695e7a12
Vortex <Thederpsnail@gmail.com>
2022-12-31 04:37:13 -0800

    Added some comments to old code

--- Items/Earth/SporeSprayer.cs
+++ Items/Earth/SporeSprayer.cs
@@ -40,21 +40,24 @@ namespace SOTS.Items.Earth
         }
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            velocity.X += Main.rand.NextFloat(-1f, 1f);
-            velocity.Y += Main.rand.NextFloat(-1f, 1f);
+            velocity.X += Main.rand.NextFloat(-1f, 1f); //Using the Terraria Main.rand library to generate a random decimal number from -1 to 1 (inclusive)
+            velocity.Y += Main.rand.NextFloat(-1f, 1f); //These two methods serve to somewhat randomize the trajectory of the arrow projectile while keeping it mostly straight
             bool field = false;
-            for (int j = -1; j <= 1; j += 2)
-                for (int i = 0; i < 3; i++)
+            for (int j = -1; j <= 1; j += 2) //This runs the following for loop twice. Once for j = -1, and once for j = 1
+            {
+                for (int i = 0; i < 3; i++) //This repeats the following thrice, changing the way an angle is modified each time
                 {
-                    if(!Main.rand.NextBool(3))
+                    if (!Main.rand.NextBool(3)) //This functions provides a roughly 2/3 computer-generated chance for the following to activate. This is because it inverses a 1/X chance function.
                     {
-                        Vector2 burstDirection = velocity.RotatedBy(MathHelper.ToRadians((7f + 35 * i) * j));
+                        Vector2 burstDirection = velocity.RotatedBy(MathHelper.ToRadians((7f + 35 * i) * j)); //Manipulate the velocity to angle the direction of the spore clouds
                         Projectile.NewProjectile(source, position, burstDirection, ModContent.ProjectileType<SporeCloudFriendly>(), (int)(damage * 0.66f), knockback, player.whoAmI);
-                        field = true;
+                        //The above function runs the necessary code to generate the projectile in game. I did not make this.
+                        field = true; //true if the item decides to generate spore clouds
                     }
                 }
-            if(field)
-                SOTSUtils.PlaySound(SoundID.Item34, (int)position.X, (int)position.Y, 0.7f, -0.1f);
+            }
+            if(field) //play spore-cloud sound (which would only not happen if the extremely low odds of (1/3)^6 are met)
+                SOTSUtils.PlaySound(SoundID.Item34, (int)position.X, (int)position.Y, 0.7f, -0.1f); //Helper method I made that calls related sound-producing functions
             return true; 
         }
     }
--- NPCs/LostSoul.cs
+++ NPCs/LostSoul.cs
@@ -20,7 +20,7 @@ namespace SOTS.NPCs
 		}
 		public override void SetDefaults()
 		{
-            NPC.aiStyle =44;
+            NPC.aiStyle = 44;
             NPC.lifeMax = 55;
             NPC.damage = 40; 
             NPC.defense = 3;	
@@ -42,17 +42,20 @@ namespace SOTS.NPCs
 		}
 		public override void AI()
 		{
-			int num1 = Dust.NewDust(new Vector2(NPC.position.X, NPC.position.Y), 28, 24, ModContent.DustType<LostSoulDust>());
+            #region generating dust with existing methods
+            int num1 = Dust.NewDust(new Vector2(NPC.position.X, NPC.position.Y), 28, 24, ModContent.DustType<LostSoulDust>());
 			Main.dust[num1].noGravity = true;
 			Main.dust[num1].velocity.X = NPC.velocity.X;
 			Main.dust[num1].velocity.Y = -3;
 			Main.dust[num1].alpha = NPC.alpha;
 			NPC.dontTakeDamage = false;
-			
-			if(ai2 == 0)
+			#endregion
+
+			//The following two if statements control the method by which the Non-Player-Character's (NPC) transparency changes
+			if (ai2 == 0)
 			{
-				NPC.alpha++;
-				if(NPC.alpha >= 235)
+				NPC.alpha++; //Alpha is a measurement of transparency in the RGBA color/drawing system
+				if (NPC.alpha >= 235) //When transparency is above 234/255, swap mode the ai2 variable, which begins the process of turning more opaque
 				{
 					ai2 = -1;
 				}
@@ -60,23 +63,23 @@ namespace SOTS.NPCs
 			if(ai2 == -1)
 			{
 				NPC.alpha--;
-				if(NPC.alpha <= 20)
+				if(NPC.alpha <= 20) //When transparancy is below 21/255, swap back to increasing transparency
 				{
 					ai2 = 0;
 				}
 			}
 			ai1++;
-			NPC.rotation = 0;
+			NPC.rotation = 0; //Prevents the NPC from changing its rotation, which is necessary as the AI it inherits from changes in a way innappropriate for this NPC
 		}
 		int frame = 0;
 		public override void FindFrame(int frameHeight) 
 		{
 			frame = frameHeight;
-			if (ai1 >= 5f) 
+			if (ai1 >= 5f) //ai1 is a variable that functions as a timer for the NPC's animations
 			{
 				ai1 -= 5f;
 				NPC.frame.Y += frame;
-				if(NPC.frame.Y >= 6 * frame)
+				if(NPC.frame.Y >= 6 * frame) //The NPC has 6 total animation frames, which is demonstrated by the animation cycling the moment the nonexistant 7th frame is met
 				{
 					NPC.frame.Y = 0;
 				}
