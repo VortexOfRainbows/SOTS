2d523d1414f1d7f78d86f781f43a343e695b9fcf
Vortex <Thederpsnail@gmail.com>
2023-03-19 20:29:23 -0700

    Some sprites and tiles
    and tiles

---
+++ Items/Conduit/ConduitChasisTile.cs
@@ -0,0 +1,58 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SOTS.Dusts;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SOTS.Items.Conduit
+{
+	public class ConduitChasis : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			this.SetResearchCost(100);
+		}
+		public override void SetDefaults()
+		{
+			Item.CloneDefaults(ItemID.StoneBlock);
+			Item.rare = ItemRarityID.Blue;
+			Item.Size = new Vector2(36, 36);
+			Item.value = Item.buyPrice(0, 0, 5, 0);
+			Item.createTile = ModContent.TileType<ConduitChasisTile>();
+		}
+	}
+	public class ConduitChasisTile : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.tileSolid[Type] = true;
+			Main.tileBrick[Type] = true;
+			Main.tileMergeDirt[Type] = false;
+			Main.tileBlockLight[Type] = false; //sunlight passes through these pipes
+			Main.tileLighted[Type] = true;
+			ItemDrop = ModContent.ItemType<ConduitChasis>();
+			AddMapEntry(new Color(66, 77, 93));
+			MineResist = 1.5f;
+			HitSound = SoundID.Tink;
+			DustType = DustID.Lead;
+			TileID.Sets.GemsparkFramingTypes[Type] = Type;
+			TileID.Sets.DrawsWalls[Type] = true;
+		}
+		public override bool PreDraw(int i, int j, SpriteBatch spriteBatch)
+		{
+			Main.tileNoSunLight[Type] = false;
+			return true;
+		}
+		public override bool Slope(int i, int j)
+		{
+			return false;
+		}
+		public override bool TileFrame(int i, int j, ref bool resetFrame, ref bool noBreak)
+		{
+			Framing.SelfFrame8Way(i, j, Main.tile[i, j], resetFrame);
+			return false;
+		}
+	}
+}
