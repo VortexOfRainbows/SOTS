d83bc7dc0be21f750808cd05321c16150ebe3acf
Vortex <vortexsnail12@gmail.com>
2024-12-11 16:18:21 -0800

    just a bit more work on sanctuary

--- WorldgenHelpers/SanctuaryWorldgenHelper.cs
+++ WorldgenHelpers/SanctuaryWorldgenHelper.cs
@@ -30,27 +30,50 @@ namespace SOTS.WorldgenHelpers
 {
     public static class SanctuaryWorldgenHelper
     {
+        public static int Ceiling => Main.UnderworldLayer + 65;
+        public static int Bottom => Main.maxTilesY - 1;
+        public static int UnderworldHeight => Main.UnderworldLayer + 65;
+        public static int SideOfWorld => Main.maxTilesX * 11 / 12;
+        public static void PrepareUnderworldArea(int x, int y, int endX, int endY)
+        {
+
+        }
         public static void GenerateSanctuary()
         {
-            int ceiling = Main.UnderworldLayer;
-            int underworldHeight = Main.UnderworldLayer + 65;
-            int rightSideOfWorld = Main.maxTilesX * 11 / 12;
-            int bottom = Main.maxTilesY - 1;
-            GenerateRectangle(rightSideOfWorld - 10, ceiling, rightSideOfWorld + 10, underworldHeight - 30);
-            GenerateRectangle(rightSideOfWorld - 10, underworldHeight, rightSideOfWorld + 10, bottom);
+            int size = 5;
+            int spread = 55;
+            for(int i = -size; i <= size; i++)
+            {
+                GeneratePillar(SideOfWorld + i * spread, UnderworldHeight);
+            }
+            GenerateRectangle(SideOfWorld - size * spread - 5, Ceiling, SideOfWorld + size * spread + 5, Ceiling + 15);
         }
-        public static void GenerateRectangle(int x, int y, int endX, int endY)
+        public static void GeneratePillar(int i, int j)
+        {
+            GenerateRectangle(i - 10, j - 30 - 1, i + 10, j + 1, -1);
+            GenerateRectangle(i - 10, Ceiling, i + 10, j - 30);
+            GenerateRectangle(i - 10, j, i + 10, Bottom);
+        }
+        public static void GenerateRectangle(int x, int y, int endX, int endY, int style = 0)
         {
             ushort Evostone = (ushort)ModContent.TileType<EvostoneBrickTile>();
-            for(int i = x; i <= endX; i++)
+            ushort EvostoneWall = (ushort)ModContent.WallType<EvostoneBrickWallTile>();
+            for (int i = x; i <= endX; i++)
             {
                 for (int j = y; j <= endY; j++)
                 {
                     Tile t = Main.tile[i, j];
                     t.ClearTile();
-                    t.TileType = Evostone;
-                    t.HasTile = true;
-                    WorldGen.TileFrame(i, j);
+                    if (style == 0)
+                    {
+                        t.TileType = Evostone;
+                        t.HasTile = true;
+                        WorldGen.TileFrame(i, j);
+                    }
+                    if (i > x && i < endX && j > y && j < endY)
+                    {
+                        t.WallType = EvostoneWall;
+                    }
                 }
             }
         }
