4220f9b5fa5e3479b06ea962198b700299025fac
Vortex <vortexsnail12@gmail.com>
2025-02-26 18:34:17 -0800

    alternate planetarium theme

--- Helpers/MusicHelper.cs
+++ Helpers/MusicHelper.cs
@@ -11,6 +11,7 @@ namespace SOTS.Helpers
         public static int AltPutridPinky => MusicLoader.GetMusicSlot("SOTS/Sounds/Music/BananaLizard/AltPutridPinky");
         public static int AltAdvisor => MusicLoader.GetMusicSlot("SOTS/Sounds/Music/BananaLizard/AltAdvisor");
         public static int AltPyramid => MusicLoader.GetMusicSlot("SOTS/Sounds/Music/BananaLizard/AltPyramid");
+        public static int AltPlanetarium => MusicLoader.GetMusicSlot("SOTS/Sounds/Music/BananaLizard/Observatory");
         public static SOTSConfig Config => SOTS.Config;
         public static int Glowmoth => MusicLoader.GetMusicSlot("SOTS/Sounds/Music/Glowmoth");
         public static int PutridPinky => Config.alternativeMusic ? AltPutridPinky : MusicLoader.GetMusicSlot("SOTS/Sounds/Music/PutridPinky");
@@ -20,7 +21,7 @@ namespace SOTS.Helpers
         public static int SubspaceSerpent => MusicLoader.GetMusicSlot("SOTS/Sounds/Music/SubspaceSerpent");
         public static int Pyramid => Config.alternativeMusic ? AltPyramid : MusicLoader.GetMusicSlot("SOTS/Sounds/Music/CursedPyramid");
         public static int PyramidBattle => MusicLoader.GetMusicSlot("SOTS/Sounds/Music/PyramidBattle");
-        public static int Planetarium => MusicLoader.GetMusicSlot("SOTS/Sounds/Music/Planetarium");
+        public static int Planetarium => Config.alternativeMusic ? AltPlanetarium : MusicLoader.GetMusicSlot("SOTS/Sounds/Music/Planetarium");
         public static int Knuckles => MusicLoader.GetMusicSlot("SOTS/Sounds/Music/KnucklesTheme");
         public static int Lux => MusicLoader.GetMusicSlot("SOTS/Sounds/Music/Lux");
         public static int PharaohsCurse => MusicLoader.GetMusicSlot("SOTS/Sounds/Music/PharaohsCurse");
