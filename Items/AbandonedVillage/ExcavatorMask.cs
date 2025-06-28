5673a69fbc52202e7d788f8ad726586dd7c6279f
Petaniks <petkins007@gmail.com>
2025-06-28 05:09:53 +0300

    More edits

--- Items/AbandonedVillage/ExcavatorMask.cs
+++ Items/AbandonedVillage/ExcavatorMask.cs
@@ -11,7 +11,7 @@ namespace SOTS.Items.AbandonedVillage
 		public override void SetDefaults()
 		{
 			Item.width = 38;
-			Item.height = 22;
+			Item.height = 20;
 			Item.vanity = true;
 			Item.value = Item.sellPrice(0, 1, 0, 0);
 			Item.rare = ItemRarityID.Orange;
