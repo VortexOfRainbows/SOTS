183542d1c24ffe3f0029c0f2b0fcad8b300a1dc6
AstrobitFake <121275955+AstrobitFake@users.noreply.github.com>
2025-07-25 15:27:04 +1000

    added the most basic subworld possible
    no way to access it though, activated portal isn't implemented

---
+++ Subworlds/Acedia.cs
@@ -0,0 +1,28 @@
+﻿using SubworldLibrary;
+using Terraria.WorldBuilding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+
+namespace SOTS.Subworlds
+{
+    public class Acedia : Subworld
+    {
+        public override int Width => 6000;
+        public override int Height => 2400;
+        public override List<GenPass> Tasks => new() 
+        {
+            new ExampleGenPass()
+        };
+        public override void OnLoad()
+        {
+            Main.dayTime = true;
+            Main.time = 27000;
+            Main.spawnTileX = Main.maxTilesX / 2;
+            Main.spawnTileY = 600;
+        }
+    }
+}
